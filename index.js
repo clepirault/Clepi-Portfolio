49900a0b2976920881058587dd00369e9363d204
Clemence Pirault <clepirault@gmail.com>
2021-05-19 18:20:29 +0200

    added content text in sections

--- index.js
+++ index.js
@@ -16,3 +16,39 @@ function showExperience4Details(){
   }
 }
 
+function showExperience3Details(){
+  const showExperienceText = document.querySelector('.experience3Detail');
+  if (showExperienceText.style.display === "none"){
+    showExperienceText.style.display = "block";
+  }else{
+    showExperienceText.style.display = "none";
+  }
+}
+
+function showExperience2Details(){
+  const showExperienceText = document.querySelector('.experience2Detail');
+  if (showExperienceText.style.display === "none"){
+    showExperienceText.style.display = "block";
+  }else{
+    showExperienceText.style.display = "none";
+  }
+}
+
+function showExperience1Details(){
+  const showExperienceText = document.querySelector('.experience1Detail');
+  if (showExperienceText.style.display === "none"){
+    showExperienceText.style.display = "block";
+  }else{
+    showExperienceText.style.display = "none";
+  }
+}
+
+function showExperience0Details(){
+  const showExperienceText = document.querySelector('.experience0Detail');
+  if (showExperienceText.style.display === "none"){
+    showExperienceText.style.display = "block";
+  }else{
+    showExperienceText.style.display = "none";
+  }
+}
+
