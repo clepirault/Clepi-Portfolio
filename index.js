0e8da45568d104f1e8aca849c2a4fe6bc38cdf8b
Clemence Pirault <clepirault@gmail.com>
2021-05-19 15:13:26 +0200

    experience in progress, links added, js for button display hide and show

---
+++ index.js
@@ -0,0 +1,18 @@
+function showExperience5Details(){
+  const showExperienceText = document.querySelector('.experience5Detail');
+  if (showExperienceText.style.display === "none"){
+    showExperienceText.style.display = "block";
+  }else{
+    showExperienceText.style.display = "none";
+  }
+}
+
+function showExperience4Details(){
+  const showExperienceText = document.querySelector('.experience4Detail');
+  if (showExperienceText.style.display === "none"){
+    showExperienceText.style.display = "block";
+  }else{
+    showExperienceText.style.display = "none";
+  }
+}
+
